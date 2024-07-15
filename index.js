a22ff72c76684b9352cb031462c8995a39a1c7ab
NaomiNyakaru <naomi.nyakaru@gmail.com>
2024-07-16 00:40:42 +0300

    array-reduce

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+let batteries = [
+  { type: 'ASSEMBLE_BATTERY', payload: 10 },
+  { type: 'ASSEMBLE_BATTERY', payload: 11 },
+  { type: 'ASSEMBLE_BATTERY', payload: 10 },
+];
 
-// Code your solution here
+let totalBatteries;
+
+function reducer(state = 0, action) {
+  switch (action.type) {
+    case 'ASSEMBLE_BATTERY':
+      return state + action.payload;
+    default:
+      return state;
+  }
+}
+
+totalBatteries = batteries.reduce((acc, current) => reducer(acc, current), 0);
